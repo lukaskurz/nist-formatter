6d63955a15f39e4852fbec8aa938ff4124f6f6b1
Lukas Kurz <lukaskurz5@gmail.com>
2017-12-22 13:26:17 +0100

    Finished the reformatting code

--- src/nist-formatter/NistFormatter/NistFormatter/Program.cs
+++ src/nist-formatter/NistFormatter/NistFormatter/Program.cs
@@ -16,42 +16,21 @@ namespace NistFormatter
 			{
 				var trainDirectory = Directory.GetDirectories(directory).Where(text => text.Split('\\').Last().StartsWith("train_")).First();
 				var letter = ((char)Int16.Parse(trainDirectory.Split('_').Last(), NumberStyles.AllowHexSpecifier)).ToString();
-				foreach (var file in Directory.GetFiles(trainDirectory))
+				var filestream = File.AppendText($@"{trainDirectory}\{letter}");
+				filestream.AutoFlush = true;
+				foreach (var file in Directory.GetFiles(trainDirectory).Where(name => name.EndsWith("png")))
 				{
 					Console.WriteLine(file);
 
 					Bitmap bmp = new Bitmap(file);
-					Bitmap resized = new Bitmap(ResizeImage(bmp,new Size(32,32)));
+					Bitmap resized = bmp.Resize(new Size(32, 32));
+
+					var normalized = resized.Normalize();
+					filestream.WriteLine($"{normalized};{letter};");
 				}
 			}
 			Console.ReadKey();
 		}
-		public static Image ResizeImage(Image image, Size size, bool preserveAspectRatio = true)
-		{
-			int newWidth;
-			int newHeight;
-			if (preserveAspectRatio)
-			{
-				int originalWidth = image.Width;
-				int originalHeight = image.Height;
-				float percentWidth = (float)size.Width / (float)originalWidth;
-				float percentHeight = (float)size.Height / (float)originalHeight;
-				float percent = percentHeight < percentWidth ? percentHeight : percentWidth;
-				newWidth = (int)(originalWidth * percent);
-				newHeight = (int)(originalHeight * percent);
-			}
-			else
-			{
-				newWidth = size.Width;
-				newHeight = size.Height;
-			}
-			Image newImage = new Bitmap(newWidth, newHeight);
-			using (Graphics graphicsHandle = Graphics.FromImage(newImage))
-			{
-				graphicsHandle.InterpolationMode = InterpolationMode.HighQualityBicubic;
-				graphicsHandle.DrawImage(image, 0, 0, newWidth, newHeight);
-			}
-			return newImage;
-		}
+		
 	}
 }
---
+++ src/nist-formatter/NistFormatter/NistFormatter/Util.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NistFormatter
+{
+	static class Util
+	{
+		public static Bitmap Resize(this Bitmap bmp, Size size, bool preserveAspectRatio = true)
+		{
+			int newWidth;
+			int newHeight;
+			if (preserveAspectRatio)
+			{
+				int originalWidth = bmp.Width;
+				int originalHeight = bmp.Height;
+				float percentWidth = (float)size.Width / (float)originalWidth;
+				float percentHeight = (float)size.Height / (float)originalHeight;
+				float percent = percentHeight < percentWidth ? percentHeight : percentWidth;
+				newWidth = (int)(originalWidth * percent);
+				newHeight = (int)(originalHeight * percent);
+			}
+			else
+			{
+				newWidth = size.Width;
+				newHeight = size.Height;
+			}
+			Bitmap newBmp = new Bitmap(newWidth, newHeight);
+			using (Graphics graphicsHandle = Graphics.FromImage(newBmp))
+			{
+				graphicsHandle.InterpolationMode = InterpolationMode.HighQualityBicubic;
+				graphicsHandle.DrawImage(bmp, 0, 0, newWidth, newHeight);
+			}
+			return newBmp;
+		}
+
+		public static string Normalize(this Bitmap bmp)
+		{
+			string result = "";
+			for (int x = 0; x < bmp.Width; x++)
+			{
+				for (int y = 0; y < bmp.Height; y++)
+				{
+					var pixel = bmp.GetPixel(x, y);
+					var grayscale = 0.2126 * pixel.R + 0.7152 * pixel.G + 0.0722 * pixel.B;
+					result += grayscale.ToString();
+					if (y + 1 < bmp.Height) result += ",";
+				}
+				if (x + 1 < bmp.Width) result += ",";
+			}
+
+			return result;
+		}
+	}
+}
