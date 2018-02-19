e08ec69b03b46fb1a3b51b7c4f5b2848c9503a0e
Lukas Kurz <lukaskurz5@gmail.com>
2018-02-19 01:19:58 +0100

    Added multifile support, put output on seperate thread, changed mapsymbol to dict and added a stopwatch

--- src/nn/network.py
+++ src/nn/network.py
@@ -1,14 +1,22 @@
 import numpy
+# import cudamat
 from scipy.special import expit
 import matplotlib.pyplot as plt
 import os
 import os.path
+from glob import glob
+from threading import Thread
+from time import sleep
+import time
+from stopwatch import Timer
 
 
-class network:
+class Network:
     input_hidden = []
     hidden_output = []
     learningrate = 0.1
+
+    targets = None
 
     symbols = ["30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "41", "42", "43", "44", "45", "46", "47",
                "48", "49", "4a", "4b", "4c", "4d", "4e", "4f", "50", "51", "52", "53", "54", "55", "56", "57", "58",
@@ -19,10 +27,18 @@ class network:
         self.input_hidden = numpy.random.normal(0, pow(hiddenlayer_size, -0.5), (inputlayer_size, hiddenlayer_size))
         self.hidden_output = numpy.random.normal(0, pow(outputlayer_size, -0.5), (hiddenlayer_size, outputlayer_size))
         self.learningrate = learningrate
+        self.init_map()
+
+    def init_map(self):
+        self.targets = {}
+        for index in range(len(self.symbols)):
+            target = numpy.zeros(len(self.symbols))
+            target[index] = 1
+            target = (target + 0.01) * 0.99
+            self.targets.update({self.symbols[index]: target})
 
     def train(self, start=[], target=[]):
-
-        # Current prediction
+        """Tries to predict the correct letter, compares with the target and changes weights using the error and GD"""
         input_hidden_result = expit(numpy.dot(start, self.input_hidden))
         hidden_output_result = expit(numpy.dot(input_hidden_result, self.hidden_output))
 
@@ -46,13 +62,8 @@ class network:
         return hidden_output_result
 
     def mapsymbol(self, symbol):
-        """Maps an ascii char to result array"""
-        for index in range(len(self.symbols)):
-            if bytearray.fromhex(self.symbols[index]).decode() == symbol:
-                target = numpy.zeros(len(self.symbols))
-                target[index] = 1
-                return target
-        raise ValueError("Tried to map a char that does not exist in the symbols entries")
+        """Maps an hexadecimal ascii char to a result array"""
+        return self.targets[symbol]
 
     def unmapsymbol(self, result=[]):
         """Maps the result array to an ascii char"""
@@ -62,7 +73,7 @@ class network:
                 biggest = index
         return bytearray.fromhex(self.symbols[biggest]).decode()
 
-    def save(self, path="save.txt"):
+    def save(self, path="save/save.txt"):
         if os.path.isfile(path):
             os.remove(path)
 
@@ -82,7 +93,7 @@ class network:
         fp.flush()
         fp.close()
 
-    def import_weights(self, path="save.txt"):
+    def import_weights(self, path="save/save.txt"):
         if not os.path.isfile(path):
             return
 
@@ -104,38 +115,53 @@ class network:
         fp.close()
 
 
-fp = open("C:/Github/nist-classificator/nist/by_class/output_600000.csv")
-content = fp.read()
-lines = content.split("\n")
-test = network(0.1, 64 * 64, 500)
-test.import_weights()
-for i in range(49990):
-    if i % 20 == 0:
-        print(i)
-    line = lines[i]
-    splitted = line.split(";")
-    correctResult = bytearray.fromhex(splitted[0]).decode()
-    input_data = numpy.asfarray(splitted[1].split(",")) / 255 * 0.99
-    input_data += 0.01
-    input_data -= 1.0
-    input_data *= -1
-    test.train(input_data, test.mapsymbol(correctResult))
+files = glob("./../../nist/by_class/*.csv")
+max_count = 60000.0
+current_count = 0.0
+running = True
+hypothesis = Network(0.1, 64 * 64, 10)
+hypothesis.import_weights()
 
-fp.flush()
-fp.close()
-test.save()
 
-# for i in range(10):
-#     splitted = lines[i].split(";")
-#     correctResult = bytearray.fromhex(splitted[0]).decode()
-#     input_data = numpy.asfarray(splitted[1].split(",")) / 255 * 0.99
-#     input_data += 0.01
-#     input_data -= 1.0
-#     input_data *= -1
-#     res = test.predict(input_data)
-#     plt.title("Correct:" + correctResult + " Prediction:" + test.unmapsymbol(res))
-#     plt.plot(res)
-#     plt.show()
+def write_progress():
+    while running:
+        print(str(int(current_count / max_count * 100)) + "% ~ line " + str(int(current_count)))
+        sleep(1)
+
+
+output_thread = Thread(target=write_progress)
+output_thread.start()
+
+stopwatch = Timer()
+stopwatch.start()
+
+for file in files:
+    fp = open(file)
+    file_content = fp.read()
+    lines = file_content.split("\n")
+    for line in lines:
+        current_count += 1
+        if current_count >= max_count:  # break if limit is reached
+            break
+
+        parts = line.split(";")
+        target_data = hypothesis.mapsymbol(parts[0])
+        input_data = (((numpy.asfarray(parts[1].split(","))
+                        / 255)  # normalize to range of ]0;1[
+                       + 0.01)  # so no 0 inputs exist
+                      * 0.99)  # no inputs bigger than 1
+
+        hypothesis.train(input_data, target_data)
+
+    fp.flush()
+    fp.close()
+    hypothesis.save()
+    if current_count >= max_count:
+        break
+
+print(stopwatch.stop("Time:"))
+running = False
+output_thread.join()
 
 correct = 0.0
 for i in range(1000):
@@ -145,8 +171,8 @@ for i in range(1000):
     input_data += 0.01
     input_data -= 1.0
     input_data *= -1
-    res = test.predict(input_data)
+    res = hypothesis.predict(input_data)
 
-    if correctResult == test.unmapsymbol(res):
+    if correctResult == hypothesis.unmapsymbol(res):
         correct += 1.0
 print("Performance p: " + str(float(correct / 1000)))
